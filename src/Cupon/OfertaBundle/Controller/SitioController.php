3810462dc0bd3bbe38fbba1fc0a8fdc39288148b
enric612 <enric612@gmail.com>
2014-03-16 12:37:30 +0100

    Modificada estructura para que sirva para todas las paginas estaticas y busque las plantillas y el controlador en Sitio en vez de Default. Corregido bug de respuesta 404 al acceder a sitio/{pagina}/ ahora si se accede a sitio/{pagina} se redirige a sitio/{pagina}/ con una respuesta 301

--- src/Cupon/OfertaBundle/Controller/SitioController.php
+++ src/Cupon/OfertaBundle/Controller/SitioController.php
@@ -5,7 +5,13 @@ namespace Cupon\OfertaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class SitioController extends Controller
 {
 	
+
+public function estaticaAction($pagina)
+	{
+		return $this->render('OfertaBundle:Sitio:'.$pagina.'.html.twig');
+	}
+
 }
