4b161f7ad5105e293d778210c1480a6475c23cdc
enric612 <enric612@gmail.com>
2014-03-16 04:06:06 +0100

    Creada pagina de ayuda en /ayuda

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
-    }
+	public function ayudaAction()
+	{
+		return new Response('Ayuda');
+	}
 }
