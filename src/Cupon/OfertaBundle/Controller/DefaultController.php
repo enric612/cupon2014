185f67cd96099ae6988c93ad1826c22f70ed932a
enric612 <enric612@gmail.com>
2014-03-16 12:22:05 +0100

    Modificado controlador ayuda para renderizar plantilla twig ayuda.html.twig, añadida plantilla ayuda.html.twig con contenido de ejemplo sencillo

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -9,6 +9,6 @@ class DefaultController extends Controller
 {
 	public function ayudaAction()
 	{
-		return new Response('Ayuda');
+		return $this->render('OfertaBundle:Default:ayuda.html.twig');
 	}
 }
