0e08f8f6bbadd2961fb8d39b6c4dbaa2afa7bd6b
enric612 <enric612@gmail.com>
2014-03-18 13:23:23 +0100

    Añadido repositorio OfertaRepository.php Guardar el fichero OfertaRepository.php y hacer un commit mas porque antes se me ha olvidado darle a Ctrl+S en sublime text antes de hacer el commit xD.

---
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cupon\OfertaBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class OfertaRepository extends EntityRepository
+{
+	public function findOfertaDelDia($oferta)
+	{
+		
+	}
+}
