ebf8d201566919c3d5693789ef9cfb70ab1763e0
enric612 <enric612@gmail.com>
2014-03-18 12:56:55 +0100

    Mejorado mecanismo de redireccion de la portada Añadido parametro ciudad por defecto en config.yml Añadida redireccion en el routing.yml con el parametro de la ciudad por defecto Añadido parametro en portada.html.twig para renderizar la url de la portada (path: ...)

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -4,20 +4,28 @@ namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-	public function portadaAction()
+	public function portadaAction($ciudad = null)
 	{
 		$em = $this->getDoctrine()->getManager();
 
-		$fechaPublicacion = new \DateTime('today');
+		
 
 		$oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-			'ciudad'			=> 1,
+			'ciudad'			=> $this->container->getParameter('cupon.ciudad_por_defecto'),
 			'fechaPublicacion'	=> new \DateTime('2014-03-18 23:59:59') // provisional...
 			));
 		
+		if(!$oferta){
+			throw $this->createNotFoundException(
+				"No hay ninguna oferta del dia para esta ciudad"
+			);
+			
+		}
+
 		return $this->render(
 			'OfertaBundle:Default:portada.html.twig',
 			array('oferta' => $oferta,
